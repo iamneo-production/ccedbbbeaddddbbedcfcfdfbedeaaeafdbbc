022ed08153c1efd60016c99790c7b2b9a86a80be
Iamneo <techadmin@iamneo.ai>
2023-08-10 14:25:39 +0000

    Backup Commit

--- reactapp/src/components/UI/Banner/Banner.js
+++
@@ -1,8 +0,0 @@
-const Banner=({score})=>{
-    return(
-        <div className="banner">
-            You haved answered {score} / 5 &nbsp;Correctly
-        </div>
-    )
-}
-export default Banner;
--- reactapp/src/components/UI/Button/Button.js
+++
@@ -1,7 +0,0 @@
-const Button=(props)=>{
-    const{btnName=props.children,onClick,marked}=props;
-    return(
-<button onClick={onClick} disabled={marked} className="btn">{btnName }</button>
-    )
-}
-export default Button;
