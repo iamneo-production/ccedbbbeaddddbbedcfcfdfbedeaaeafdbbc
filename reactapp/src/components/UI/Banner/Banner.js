015499be1abdb01921e7e7f729f6582137595ae5
Iamneo <techadmin@iamneo.ai>
2023-08-10 14:30:39 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Banner/Banner.js
@@ -0,0 +1,8 @@
+const Banner=({score})=>{
+    return(
+        <div className="banner">
+            You haved answered {score} / 5 &nbsp;Correctly
+        </div>
+    )
+}
+export default Banner;
